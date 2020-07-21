f2a1dbbf9254474f3ae78ddfc0c6613dac08da53
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-21 14:21:05 -0700

    does jenmkins like testing_dir?

--- selfdrive/test/setup_phone_ci.sh
+++ selfdrive/test/setup_phone_ci.sh
@@ -7,10 +7,10 @@ if [ -z "$GIT_COMMIT" ]; then
   exit 1
 fi
 
-#if [ -z "$TEST_DIR" ]; then
-#  echo "TEST_DIR must be set"
-#  exit 1
-#fi
+if [ -z "$TESTING_DIR" ]; then
+  echo "TESTING_DIR must be set"
+  exit 1
+fi
 echo "done"
 exit 0
 
@@ -31,6 +31,6 @@ git submodule foreach --recursive git reset --hard
 git submodule foreach --recursive git clean -xdf
 echo "git checkout took $SECONDS seconds"
 
-rsync -a --delete $SOURCE_DIR $TEST_DIR
+rsync -a --delete $SOURCE_DIR $TESTING_DIR
 
-echo "$TEST_DIR synced with $GIT_COMMIT, took $SECONDS seconds"
+echo "$TESTING_DIR synced with $GIT_COMMIT, took $SECONDS seconds"

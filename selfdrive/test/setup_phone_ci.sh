1dd64392dd126a030c7c0d7d3b61c9a3f576c9cf
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-21 15:02:17 -0700

    more escape

--- selfdrive/test/setup_phone_ci.sh
+++ selfdrive/test/setup_phone_ci.sh
@@ -4,12 +4,12 @@ export SOURCE_DIR="/data/openpilot_source/"
 
 printenv
 
-if [ -z "$GIT_COMMIT" ]; then
+if [ -z "\$GIT_COMMIT" ]; then
   echo "GIT_COMMIT must be set"
   exit 1
 fi
 
-if [ -z "$TEST_DIR" ]; then
+if [ -z "\$TEST_DIR" ]; then
   echo "TEST_DIR must be set"
   exit 1
 fi
@@ -21,18 +21,18 @@ exit 0
 cd /tmp && find -name 'scons_cache_*' -type d -maxdepth 1 -mtime 1 -exec rm -rf '{}' \;
 
 # set up environment
-cd $SOURCE_DIR
+cd \$SOURCE_DIR
 git reset --hard
 git fetch origin
 find . -maxdepth 1 -not -path './.git' -not -name '.' -not -name '..' -exec rm -rf '{}' \;
-git reset --hard $GIT_COMMIT
-git checkout $GIT_COMMIT
+git reset --hard \$GIT_COMMIT
+git checkout \$GIT_COMMIT
 git clean -xdf
 git submodule update --init
 git submodule foreach --recursive git reset --hard
 git submodule foreach --recursive git clean -xdf
-echo "git checkout took $SECONDS seconds"
+echo "git checkout took \$SECONDS seconds"
 
-rsync -a --delete $SOURCE_DIR $TEST_DIR
+rsync -a --delete \$SOURCE_DIR $TEST_DIR
 
-echo "$TEST_DIR synced with $GIT_COMMIT, took $SECONDS seconds"
+echo "\$TEST_DIR synced with \$GIT_COMMIT, took \$SECONDS seconds"
